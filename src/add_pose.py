0ea32b804f3fbd3440db517fca5274f766185e88
Eline Voorbraak <e.voorbraak@student.tue.nl>
2026-05-19 15:40:59 +0200

    part 8.1 done

--- src/add_pose.py
+++ src/add_pose.py
@@ -10,19 +10,17 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_pose(graph, initial_estimate):
 
-    angle_45 = np.deg2rad(45)
-    rel_x = 2.0 * np.cos(angle_45)
-    rel_y = 2.0 * np.sin(angle_45)
-    rel_theta = np.deg2rad(90)
-
-    odometry = gtsam.Pose2(rel_x, rel_y, rel_theta)
-
-    # This connects X(3) to X(4)
-    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), odometry, ODOMETRY_NOISE))
+    angle_rad = math.radians(45)
+    dx = 2.0 * math.cos(angle_rad)
+    dy = 2.0 * math.sin(angle_rad)
+    dtheta = math.radians(90) 
     
-    # Add an initial guess for X(4)
-    pose_3_guess = initial_estimate.atPose2(X(3))
-    pose_4_guess = pose_3_guess.compose(odometry)
-    initial_estimate.insert(X(4), pose_4_guess)
+    odometry_delta = gtsam.Pose2(dx, dy, dtheta)
 
+    # Add the BetweenFactor to the graph
+    # This connects the previous pose X(3) to the new pose X(4)
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), odometry_delta, ODOMETRY_NOISE))
+
+    initial_estimate.insert(X(4), gtsam.Pose2(5.4, 1.4, 1.57))
+    
     return graph, initial_estimate
